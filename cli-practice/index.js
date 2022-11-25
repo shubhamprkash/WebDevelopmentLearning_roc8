e281c50e2fad62be8522c07c25116632f331e2e7
ShubhamPrakash <shubham.prakash.1297@gmail.com>
2022-11-25 22:10:23 +0530

    cli-js practice

---
+++ cli-practice/index.js
@@ -0,0 +1,11 @@
+// ex01: output your name
+// console.log("\nName somthing\n")
+
+// ex02: read the name of your user
+/*
+const readline = require("readline-sync");
+var userName = readline.question('\nMind typing your name? ');
+console.log('\nHey ' + userName + '!\n');
+*/
+
+
