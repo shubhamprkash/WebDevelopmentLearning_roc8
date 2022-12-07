d6d9fc42a2063fd91d9194799aa46c386fa3fa89
ShubhamPrakash <56962038+shubhamprkash@users.noreply.github.com>
2022-12-07 21:42:23 +0530

    added: ex03

--- cli-practice/vanillaJS.js
+++ cli-practice/vanillaJS.js
@@ -1,4 +1,5 @@
 // prompt testing
+/*
 let person = prompt("Please enter your name", "Harry Potter");
 if (person===null) {
     confirm("r uh sure?")
@@ -6,5 +7,34 @@ if (person===null) {
 else
 console.log("hey! " + person)
 console.log("decision decision, all of them wrong! Uni uni wts the con!")
+*/
+// console.log(1+1, "1+1")
 
-console(1+1, "1+1")
+// Exercise 03: Wiring button click
+// Reference Btn
+
+/*
+let btn= document.querySelector('#btn-first');
+let btnEvent= btn.addEventListener("click", clicked);
+function clicked(){
+    console.log("Clicked Baby!");
+};
+*/
+
+//ex04: wiring textarea input
+
+// let heading =document.querySelector('#heading');
+let txtAr = document.querySelector('#textarea');
+let btn= document.querySelector('#btn-first');
+// let txtarVal = txtAr.value;
+let btnEvent= btn.addEventListener("click", clicked);
+
+function clicked(){
+    console.log(txtAr.value);
+};
+
+
+
+
+
+
